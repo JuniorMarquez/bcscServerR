03e19fb6b430467a51c96789fc1a918375571d7e
root <root@sohigh.com.ve>
2017-07-12 15:43:27 +0000

    first commit

---
+++ api/controllers/SolicitudatenController.js
@@ -0,0 +1,11 @@
+/**
+ * SolicitudatenController
+ *
+ * @description :: Server-side logic for managing solicitudatens
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
